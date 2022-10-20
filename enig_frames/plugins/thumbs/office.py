1280d241a2fcfb80ac508b706b3e76705c9883f0
root <zugeliang2000@gmail.com>
2022-10-20 17:35:47 +0700

    ok

--- enig_frames/plugins/thumbs/office.py
+++ enig_frames/plugins/thumbs/office.py
@@ -9,7 +9,11 @@ import enig_frames.services.PDFs
 import enig_frames.services.images
 import enig_frames.services.file_system
 import enig_frames.services.files
-class PDF(enig_frames.plugins.base_plugin.BasePlugin):
+import enig_frames.services.office
+
+
+
+class Office(enig_frames.plugins.base_plugin.BasePlugin):
     def __init__(
             self,
             configuration: enig_frames.config.Configuration = enig.depen(
@@ -18,9 +22,9 @@ class PDF(enig_frames.plugins.base_plugin.BasePlugin):
             file_system_utils: enig_frames.services.file_system_utils = enig.depen(
                 enig_frames.services.file_system_utils.FileSystemUtils
             ),
-            pdf_file_service: enig_frames.services.PDFs.PdfFileService = enig.depen(
-                enig_frames.services.PDFs.PdfFileService
-            ),
+            # pdf_file_service: enig_frames.services.PDFs.PdfFileService = enig.depen(
+            #     enig_frames.services.PDFs.PdfFileService
+            # ),
             image_service:enig_frames.services.images.ImageServices = enig.depen(
                 enig_frames.services.images.ImageServices
             ),
@@ -29,23 +33,29 @@ class PDF(enig_frames.plugins.base_plugin.BasePlugin):
             ),
             file_services: enig_frames.services.files.Files = enig.depen(
                 enig_frames.services.files.Files
+            ),
+            office_service : enig_frames.services.office.OfficeService =enig.depen(
+                enig_frames.services.office.OfficeService
             )
     ):
         self.configuration: enig_frames.config.Configuration = configuration
         self.file_system_utils: enig_frames.services.file_system_utils.FileSystemUtils = file_system_utils
-        self.pdf_file_service: enig_frames.services.PDFs.PdfFileService = pdf_file_service
+        # self.pdf_file_service: enig_frames.services.PDFs.PdfFileService = pdf_file_service
         self.image_service:enig_frames.services.images.ImageServices = image_service
         self.file_system_services:enig_frames.services.file_system.FileSystem = file_system_services
         self.file_services:enig_frames.services.files.Files= file_services
+        self.office_service:enig_frames.services.office.OfficeServices =office_service
         enig_frames.plugins.base_plugin.BasePlugin.__init__(self)
 
     def process(self, file_path: str, app_name: str, upload_id: str):
-        file_ext_only = self.file_system_utils.get_file_extenstion(file_path)
-        if file_ext_only.lower() == "pdf":
-            image_file: str = self.pdf_file_service.convert_to_image(
-                pdf_file=file_path
-            )
-            thumb_file = self.image_service.create_thumbs(image_file, 450)
+        mime_type = self.file_system_utils.get_mime_type(file_path)
+        file_ext = self.file_system_utils.get_file_extenstion(file_path)
+        if '-officedocument.' in mime_type \
+                or file_ext.lower() in self.configuration.config.ext_office_file \
+                or file_ext.upper() in self.configuration.config.ext_office_file:
+            file_ext_only = self.file_system_utils.get_file_extenstion(file_path)
+            image_path = self.office_service.convert_to_image(file_path)
+            thumb_file = self.image_service.create_thumbs(image_path, 450)
 
             file_name_only = self.file_system_utils.get_file_name_only(thumb_file)
             file_info = self.file_system_services.upload(
@@ -75,9 +85,9 @@ class PDF(enig_frames.plugins.base_plugin.BasePlugin):
             self.make_available_thumbs(
                 upload_id=upload_id,
                 app_name=app_name,
-                image_file=image_file,
+                image_file=image_path,
                 file_system_services=self.file_system_services,
                 file_services=self.file_services
 
             )
-            os.remove(image_file)
+            os.remove(thumb_file)
