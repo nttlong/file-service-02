48f7eeeac4859e87555412dd5722ed64ad67cb70
root <zugeliang2000@gmail.com>
2022-11-21 11:10:47 +0700

    ok

--- cy_xdoc/services/search_engine.py
+++ cy_xdoc/services/search_engine.py
@@ -5,3 +5,6 @@ class SearchEngine:
 
     def mark_delete(self, app_name, id, mark_delete_value):
         raise NotImplemented
+
+    def full_text_search(self, app_name, content, page_size, page_index, highlight):
+        raise NotImplemented
