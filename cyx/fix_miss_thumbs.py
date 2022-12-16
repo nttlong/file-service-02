80c42e34f03addce27fbf70824f6842bbe10b0c1
root <zugeliang2000@gmail.com>
2022-12-16 14:01:51 +0700

    ok

--- cyx/fix_miss_thumbs.py
+++ cyx/fix_miss_thumbs.py
@@ -5,12 +5,12 @@ from cy_xdoc.models.files import DocUploadRegister
 from cyx.common.msg_mongodb import MessageServiceMongodb
 fs:FileServices = cy_kit.singleton(FileServices)
 msg:MessageServiceMongodb =cy_kit.singleton(MessageServiceMongodb)
-app_name = "lv-docs"
+app_name = "hps-file-test"
 
 context = fs.db_connect.db(app_name).doc(DocUploadRegister)
 
 lst = context.context.aggregate().match(
-    (context.fields.FileExt == "pdf")
+    (context.fields.id == "31747044-7484-4674-b013-b39d3e2e9b30")
 
 ).sort(
     context.fields.RegisterOn.desc()
