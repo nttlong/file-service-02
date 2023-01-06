6bcc19c98fb8967c27f45e49baf111b366d3d978
root <zugeliang2000@gmail.com>
2023-01-06 13:35:34 +0700

    ok

--- cy_xdoc/controllers/files_content_thumbs.py
+++ cy_xdoc/controllers/files_content_thumbs.py
@@ -4,6 +4,7 @@ from fastapi import Response,Request
 from fastapi.responses import FileResponse
 import cyx.common.file_storage
 import mimetypes
+import cy_xdoc.services.files
 @cy_web.hanlder(method="get",path="{app_name}/thumbs/{directory:path}")
 async def get_thumb_of_files(app_name: str, directory: str,request:Request):
     """
@@ -12,6 +13,7 @@ async def get_thumb_of_files(app_name: str, directory: str,request:Request):
     :return:
     """
     file_storage_service = cy_kit.singleton(cyx.common.file_storage.FileStorageService)
+
     thumb_dir_cache = os.path.join(app_name, "custom_thumbs")
     cache_thumb_path = cy_web.cache_content_check(thumb_dir_cache, directory.lower().replace("/", "_"))
     if cache_thumb_path:
@@ -23,9 +25,27 @@ async def get_thumb_of_files(app_name: str, directory: str,request:Request):
         rel_file_path=f"thumbs/{directory}"
     )
     if fs is None:
-        return Response(
-            status_code=401
-        )
+        thumb_dir_cache = os.path.join(app_name, "custom_thumbs")
+        cache_thumb_path = cy_web.cache_content_check(thumb_dir_cache, directory.lower().replace("/", "_"))
+        if cache_thumb_path:
+            return FileResponse(cache_thumb_path)
+
+        upload_id = directory.split('/')[0]
+
+        service_file = cy_kit.singleton(
+            cy_xdoc.services.files.FileServices)
+        fs = service_file.get_main_main_thumb_file(app_name, upload_id)
+        if fs is None:
+            return Response(
+                status_code=401
+            )
+        content = fs.read(fs.get_size())
+        fs.seek(0)
+        cy_web.cache_content(thumb_dir_cache, directory.replace('/', '_'), content)
+        del content
+        mime_type, _ = mimetypes.guess_type(directory)
+        ret = await cy_web.cy_web_x.streaming_async(fs, request, mime_type)
+        return ret
     content = fs.read(fs.get_size())
     fs.seek(0)
     cy_web.cache_content(thumb_dir_cache, directory.replace('/', '_'), content)
